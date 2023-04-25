2ba7166db305462d5159e11f43d580caae215f63
RemyDesa <131722984+RemyDesa@users.noreply.github.com>
2023-04-25 12:36:55 +0200

    Add files via upload

--- Dijkstra/Dijkstra.java
+++ Dijkstra/Dijkstra.java
@@ -17,20 +17,25 @@ public class Dijkstra<T,G extends Number> {
   public Map<T, G> dijkstra(Graph<T, G> graph, T start, Comparator<Edge<T, G>> comparator, G startValue,G inf) throws Exception {
     Map<T,G> distances = new HashMap<>();   //Distances from nodes
     MinHeap<Edge<T,G>> pq = new MinHeap<>(comparator); //Priority Queue
-    ArrayList<T> keys = graph.getNodes();    //All the nodes names from the graph
+    List<T> keys = graph.getNodes();    //All the nodes names from the graph
     Edge<T, G> u;
-    for (T i : keys) {  //O(n)
-      if (i.equals(start)) {
-        distances.put(start, startValue);
-        pq.insert(new Edge<>(start, startValue));
-      } else {
-        distances.put(i, inf);
-        pq.insert(new Edge<>(i, inf));
+    // Initialize distances and add all nodes to the priority queue
+      for (T node : graph.getNodes()) {
+          G support;
+          if (node.equals(start)) {
+              distances.put(start, startValue);
+              support=startValue;
+          } else {
+              distances.put(node, inf);
+              support=inf;
+          }
+          pq.insert(new Edge<>(node, distances.get(node),support));
       }
-    }
+      /*
     while (!pq.isEmpty()) { //O(n)
       u = pq.extractMin();    //O(log n)
       //System.out.println("EDGE MIN "+u.toString());
+      
       int j=0;
       for(Edge<T,G> i : graph.getAdjacentVertex(u.getEnd())) {    //O(m) for dense graph O(1)
         System.out.println(j);
@@ -38,7 +43,7 @@ public class Dijkstra<T,G extends Number> {
         if(pq.contains(new Edge<>(i.getEnd(),distances.get(i.getEnd()))) &&
           Edge.compare(distances.get(i.getEnd()),Edge.add(u.getWeight(),graph.getEdgeLabel(u.getEnd(),i.getEnd()))) > 0) {
           //decrease the actual value in min heap for newer value O(log n)
-          //System.out.println("Sono dentro if con "/*+distances.get(i.getEnd())+" e "+Edge.add(u.getWeight(),graph.getEdgeLabel(u.getEnd(),i.getEnd())*/);
+          //System.out.println("Sono dentro if con "/*+distances.get(i.getEnd())+" e "+Edge.add(u.getWeight(),graph.getEdgeLabel(u.getEnd(),i.getEnd()));
           pq.decreaseKey(new Edge<>(i.getEnd(),distances.get(i.getEnd())),
             new Edge<>(i.getEnd(),Edge.add(u.getWeight(),graph.getEdgeLabel(u.getEnd(),i.getEnd()))));
           //update the value of the edge
@@ -50,6 +55,26 @@ public class Dijkstra<T,G extends Number> {
     //System.out.println(Collections.singletonList(distances));
     System.out.println(Arrays.asList(distances));
     return distances;
+    */
+    while (!pq.isEmpty()) {
+            Edge<T, G> curr = pq.extractMin(); // Extract node with minimum distance
+            G currDist = distances.get(curr.getDest()); // Current distance to curr node
+
+            // Update distances to adjacent nodes
+            for (Edge<T, G> edge : graph.getAdjacentNodes(curr.getDest())) {
+                T adjNode = edge.getDest();
+                G newDist = Edge.add(currDist, edge.getLabel()); // New distance to adjNode
+
+                if (pq.contains(new Edge<>(adjNode, distances.get(adjNode))) &&
+                        Edge.compare(newDist, distances.get(adjNode)) < 0) {
+                    // Update distance to adjNode and decrease key in priority queue
+                    distances.put(adjNode, newDist);
+                    pq.decreaseKey(new Edge<>(adjNode, distances.get(adjNode)), new Edge<>(adjNode, newDist));
+                }
+            }
+        }
+
+        return distances;
   }
 }
 
